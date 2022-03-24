3e579f730fc694620fcab10f71670bdec58ae247
BryceTuppurainen <bryce.tuppurainen.au@gmail.com>
2022-03-25 09:40:22 +1100

    A few more styling changes

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -41,17 +41,12 @@ class Contact extends React.Component {
 
   render() {
     return (
-      <article class="bg-gradient-to-br from-gray-900 via-black to-slate-800 py-3">
+      <article class="bg-gradient-to-tr from-zinc-800 to-black py-3">
         <div class="ml-9">
-          <a href="#contact">
-            <h2
-              id="contact"
-              class="hover:underline py-6 text-5xl text-slate-100 font-semibold smvp:text-3xl"
-            >
-              Contact
-            </h2>
-          </a>
-          <p class="text-2xl mb-6">
+          <h2 class="py-6 text-5xl smvp:text-3xl text-slate-100 font-semibold">
+            Contact
+          </h2>
+          <p class="text-2xl smvp:text-xl mb-6">
             <a
               href="mailto:bryce@tuppurainen.com.au"
               class="hover:underline text-slate-100"
@@ -61,14 +56,14 @@ class Contact extends React.Component {
           </p>
           <form name="contact" netlify onSubmit={this.handleSubmit}>
             <input type="hidden" name="form-name" value="contact" />
-            <div class="flex flex-wrap gap-3 min-w-11/12">
+            <div class="flex flex-wrap gap-3 w-full">
               <div class="w-full flex gap-3">
-                <label class="text-slate-100 text-2xl inline-block w-[10rem] ">
+                <label class="smvp:w-[5rem] text-slate-100 text-2xl inline-block w-[10rem] ">
                   Name*
                 </label>
                 <input
                   type="text"
-                  class="w-[20rem] smvp:w-[20rem] text-black p-3 border border-black rounded text-lg "
+                  class="w-[20rem] smvp:w-[15rem] text-black p-3 border border-black rounded text-lg "
                   onChange={this.handleChange}
                   value={this.state.firstname}
                   name="firstname"
@@ -77,7 +72,7 @@ class Contact extends React.Component {
                 />
                 <input
                   type="text"
-                  class="w-[20rem] smvp:w-[20rem] text-black p-3 border border-black rounded text-lg "
+                  class="w-[20rem] smvp:w-[15rem] text-black p-3 border border-black rounded text-lg "
                   onChange={this.handleChange}
                   value={this.state.lastname}
                   name="lastname"
@@ -87,26 +82,26 @@ class Contact extends React.Component {
               </div>
 
               <div class="w-full flex gap-3">
-                <label class="text-slate-100 text-2xl inline-block w-[10rem] ">
+                <label class="smvp:w-[5rem] text-slate-100 text-2xl inline-block w-[10rem] ">
                   Email*
                 </label>
                 <input
                   type="text"
-                  class="w-[40.75rem] smvp:w-[40.75rem] text-black p-3 border border-black rounded text-lg "
+                  class="w-[40.75rem] smvp:w-[30.75rem] text-black p-3 border border-black rounded text-lg "
                   onChange={this.handleChange}
                   value={this.state.email}
                   name="email"
-                  placeholder="place.holder@gmail.com"
+                  placeholder="placeholder@example.com"
                   required
                 />
               </div>
 
               <div class="w-full flex gap-3">
-                <label class="text-slate-100 text-2xl inline-block w-[10rem]  align-top">
-                  Enquiry*
+                <label class="smvp:w-[5rem] text-slate-100 text-2xl inline-block w-[10rem]  align-top">
+                  Enquiry
                 </label>
                 <textarea
-                  class="w-[40.75rem] smvp:w-[40.75rem] text-black p-3 border border-black rounded text-lg h-[20rem]"
+                  class="w-[40.75rem] smvp:w-[30.75rem] text-black p-3 border border-black rounded text-lg h-[20rem]"
                   onChange={this.handleChange}
                   value={this.state.content}
                   name="content"
@@ -118,7 +113,7 @@ class Contact extends React.Component {
                 <div class="inline-block w-[10rem]"></div>
                 <input
                   type="submit"
-                  class="text-black p-3 border border-black rounded bg-white hover:bg-slate-600 hover:border-white hover:text-white hover:cursor-pointer"
+                  class="text-black p-3 border border-black rounded bg-white hover:bg-zinc-700 hover:border-white hover:text-white hover:cursor-pointer"
                   value="Send Message"
                 />
               </div>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,3 +1,4 @@
+const typescriptLogo = require("../img/typescript-logo.png");
 const reactLogo = require("../img/react-logo.png");
 const tailwindLogo = require("../img/tailwind-logo.png");
 const netlifyLogo = require("../img/netlify-logo.png");
@@ -6,17 +7,52 @@ function Header(props) {
   return (
     <header>
       <div class="min-h-[70vh] content-center justify-center text-center">
-        <h2 class="p-6 pt-20 text-lg text-slate-100 font-bold">
-          tuppurainen.com.au
-        </h2>
+        <a href="https://tuppurainen.com.au/" target="_self">
+          <h2 class="p-6 pt-20 text-lg text-slate-100 font-bold hover:underline">
+            tuppurainen.com.au
+          </h2>
+        </a>
         <h1 class="p-6 text-7xl text-slate-100 font-bold smvp:text-3xl">
           Bryce Tuppurainen
         </h1>
         <h2 class="p-6 text-sm text-slate-200 mb-20">
-          A portfolio site by Bryce Tuppurainen developed using
-          <span class="font-bold"> ReactJS</span>,
-          <span class="font-bold"> TailwindCSS</span>, and deployed using
-          <span class="font-bold"> Netlify</span>
+          A portfolio site by Bryce Tuppurainen developed using{" "}
+          <a
+            href="https://reactjs.org/"
+            rel="noreferrer"
+            target={"_blank"}
+            class="font-bold hover:underline"
+          >
+            ReactJS
+          </a>
+          ,{" "}
+          <a
+            href="https://tailwindcss.com/"
+            rel="noreferrer"
+            target={"_blank"}
+            class="font-bold hover:underline"
+          >
+            TailwindCSS
+          </a>
+          , and deployed using{" "}
+          <a
+            href="https://www.netlify.com/"
+            rel="noreferrer"
+            target={"_blank"}
+            class="font-bold hover:underline"
+          >
+            Netlify
+          </a>
+          . Written in{" "}
+          <a
+            href="https://www.typescriptlang.org/"
+            rel="noreferrer"
+            target={"_blank"}
+            class="font-bold hover:underline"
+          >
+            TypeScript
+          </a>
+          .
         </h2>
         <div class="pb-8">
           <p class="text-xs">This site's development-stack:</p>
@@ -38,6 +74,17 @@ function Header(props) {
             <img
               src={netlifyLogo}
               alt="Netlify logo"
+              class="inline-block w-[8rem] h-[8rem] rounded-2xl border-sky-300 hover:border hover:rounded-3xl align-top m-3 translate-y-1"
+            ></img>
+          </a>
+          <a
+            href="https://www.typescriptlang.org/"
+            rel="noreferrer"
+            target={"_blank"}
+          >
+            <img
+              src={typescriptLogo}
+              alt="TypeScript logo"
               class="inline-block w-[8rem] h-[8rem] rounded-2xl border-sky-300 hover:border hover:rounded-3xl align-top m-3 translate-y-1"
             ></img>
           </a>
